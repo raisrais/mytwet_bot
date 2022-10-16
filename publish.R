619755c7f97aa56912edf77306c4625c4e67b071
raisrais <ipb21_rais@apps.ipb.ac.id>
2022-10-17 06:44:44 +0700

    upload

---
+++ publish.R
@@ -0,0 +1,55 @@
+library(RPostgreSQL)
+
+drv <- dbDriver("PostgreSQL")
+
+con <- dbConnect(drv,
+                 dbname = Sys.getenv("ELEPHANT_SQL_DBNAME"), 
+                 host = Sys.getenv("ELEPHANT_SQL_HOST"),
+                 port = 5432,
+                 user = Sys.getenv("ELEPHANT_SQL_USER"),
+                 password = Sys.getenv("ELEPHANT_SQL_PASSWORD")
+)
+
+query2 <- '
+SELECT * FROM "public"."INFOGRAPHIC"
+'
+
+data <- dbGetQuery(con, query2)
+
+status_details <- paste0(
+  data$Judul[nrow(data)], ".", "\n",
+  "Infographic category : ", data$Kategori[nrow(data)], "\n",
+  "Please visit ", data$Tautan[nrow(data)], " for detail.",  "\n",
+  "\n",
+  "#","dailyinfographic"
+)
+
+# Publish to Twitter
+library(rtweet)
+
+## Create Twitter token
+pangan_token <- rtweet::rtweet_bot(
+  api_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  api_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+## Provide alt-text description
+alt_text <- paste0(
+  "Daily Infographic | Learn something new everyday"
+)
+
+# save the data
+dir.create(file.path('data') )
+write.csv(data, file.path("data/da_infographic.csv"))
+
+# Post tweet
+rtweet::post_tweet(
+  status = status_details,
+  media = NULL,
+  media_alt_text = alt_text,
+  token = twitter_token
+)
+
+on.exit(dbDisconnect(con))
---
+++ save.R
@@ -0,0 +1,73 @@
+library(RPostgreSQL)
+
+drv <- dbDriver("PostgreSQL")
+
+con <- dbConnect(drv,
+                 dbname = Sys.getenv("ELEPHANT_SQL_DBNAME"), 
+                 host = Sys.getenv("ELEPHANT_SQL_HOST"),
+                 port = 5432,
+                 user = Sys.getenv("ELEPHANT_SQL_USER"),
+                 password = Sys.getenv("ELEPHANT_SQL_PASSWORD")
+)
+
+# Memanggil Tabel, untuk membuat Primary Key nya berurutan.
+query2 <- '
+SELECT * FROM "public"."INFOGRAPHIC"
+'
+data <- dbGetQuery(con, query2)
+
+library(rvest)
+
+# membaca environment variabel
+twitter_token <- rtweet::rtweet_bot(
+  api_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  api_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+# Konten yang akan di-posting
+# Spesifikasi URL dari website yang akan diambil informasinya
+url <- 'https://dailyinfographic.com/'
+
+#Membaca html dari website
+webpage <- read_html(url)
+
+category <- html_nodes(webpage,'.des>.cat>a') %>% 
+  html_text("a")
+category
+
+title <- html_nodes(webpage,'h1>a') %>% 
+  html_attr("title")
+title
+
+category <- html_nodes(webpage,'.des>.cat>a') %>% 
+  html_text("a")
+category
+
+#link selengkapnya
+library(urlshorteneR)
+link <- html_nodes(webpage,'h1>a')%>%
+  html_attr("href")
+
+bitly_shorten_link(
+  domain = "bit.ly",
+  group_guid = NULL,
+  long_url = link,
+  showRequestURL = TRUE
+)
+
+shorten <- bitly_shorten_link(long_url = link, showRequestURL = TRUE)
+short_link <- as.character(shorten$id)
+
+baris <- nrow(data)
+baris
+data <- data.frame(No = baris+1,
+                   Tanggal = Sys.Date(),
+                   Judul = title,
+                   Kategori = category,
+                   Tautan = short_link)
+
+dbWriteTable(conn = con, name = "INFOGRAPHIC", value = data, append = TRUE, row.names = FALSE, overwrite=FALSE)
+
+on.exit(dbDisconnect(con))
